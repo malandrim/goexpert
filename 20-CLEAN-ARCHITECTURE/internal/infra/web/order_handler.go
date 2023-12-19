4bcf68d0664d991769f7605deaf79c554e3869c9
malandrim <lh.malandrim@gmail.com>
2023-12-19 01:14:11 -0300

    Updading Folder name item 20

---
+++ 20-CLEAN-ARCHITECTURE/internal/infra/web/order_handler.go
@@ -0,0 +1,12 @@
+package web
+
+import (
+	"github.com/malandrim/goexpert/20-CleanArchitecture/internal/entity"
+	"github.com/malandrim/goexpert/20-CleanArchitecture/internal/pkg/events"
+)
+
+type WebOrderHandler struct {
+	EventDispatcher  events.EventDispatcherInterface
+	OrderRepository  entity.OrderRepositoryInterface
+	OrderCreateEvent events.EventInterface
+}
